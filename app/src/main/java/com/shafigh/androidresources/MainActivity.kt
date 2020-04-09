b4fa2ea58a6eac6e96669f5b1eec957d8bbee52a
Shafigh Khalili <shafiqkhalili@gmail.com>
2020-04-09 10:48:33 +0200

    Translate String

--- app/src/main/java/com/shafigh/androidresources/MainActivity.kt
+++ app/src/main/java/com/shafigh/androidresources/MainActivity.kt
@@ -2,11 +2,21 @@ package com.shafigh.androidresources
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var greetingsTextview : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        greetingsTextview = findViewById(R.id.textView)
+
+    }
+    fun buttonPressed(view:View){
+        greetingsTextview.text = getString(R.string.thank_you)
     }
 }
